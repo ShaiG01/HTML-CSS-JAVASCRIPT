86f0c2fe58409a29176eb3bc645e341343eaf55d
Shaikira <Shaigonzales39@gmail.com>
2025-06-08 14:12:09 +0800

    Update create.js

--- HTML AND CSS PROJECTS/toDo/create.js
+++ HTML AND CSS PROJECTS/toDo/create.js
@@ -1,17 +1,1 @@
-const overlayInput = document.querySelector('.overlayInput')
-const overlay = document.querySelector('.overlay')
-const addNewBtn = document.querySelector('.addNew')
-const xBtn = document.querySelector('.x')
 
-
-addNewBtn.addEventListener('click', ()=>{
-    const currentScale = parseInt(getComputedStyle(overlayInput).getPropertyValue('--scale')) || 0;
-    overlayInput.style.setProperty('--scale', `${currentScale + 1}`)
-    overlay.classList.add('hide')
-})
-
-xBtn.addEventListener('click', ()=>{
-     const currentScale = parseInt(getComputedStyle(overlayInput).getPropertyValue('--scale')) || 0;
-    overlayInput.style.setProperty('--scale', `${currentScale - 1}`)
-    overlay.classList.remove('hide')
-})
